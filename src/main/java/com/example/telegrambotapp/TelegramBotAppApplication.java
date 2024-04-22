23e58f78cbd2da8d1dc7490f4f71ab2a3227ff81
Ata Ayyıldız <84992313+satas20@users.noreply.github.com>
2024-04-22 10:12:42 +0300

    İnitial Commit

---
+++ src/main/java/com/example/telegrambotapp/TelegramBotAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotAppApplication.class, args);
+    }
+
+}
